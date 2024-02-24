48ad1ac3595f2cee2352bbf0ade6b264e0cf540a
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-02-23 22:35:29 -0300

    feat:encrypts cookie in base64 so no characters are lost

--- cookies/cookies.go
+++ cookies/cookies.go
@@ -3,13 +3,14 @@ package cookies
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func SetCookie(w http.ResponseWriter, r *http.Request) {
 	cookie := http.Cookie{
 		Name:     "auth",
-		Value:    "Hello world",
+		Value:    "Hello Zoë!",
 		Path:     "/",
 		MaxAge:   3600,
 		HttpOnly: true,
@@ -17,21 +18,27 @@ func SetCookie(w http.ResponseWriter, r *http.Request) {
 		SameSite: http.SameSiteLaxMode,
 	}
 
-	http.SetCookie(w, &cookie)
+	if err := write(w, cookie); err != nil {
+		log.Println(err)
+		http.Error(w, "server error", http.StatusInternalServerError)
+		return
+	}
 
 	w.Write([]byte(fmt.Sprintf("cookie %v set", cookie.Name)))
 }
 
 func GetCookie(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("auth")
+	cookie, err := read(r, "auth")
 	if err != nil {
 		switch {
 		case errors.Is(err, http.ErrNoCookie):
 			http.Error(w, "cookie not found", http.StatusBadRequest)
+			return
 		default:
 			http.Error(w, "server error", http.StatusInternalServerError)
+			return
 		}
 	}
 
-	w.Write([]byte(cookie.Value))
+	w.Write([]byte(cookie))
 }
